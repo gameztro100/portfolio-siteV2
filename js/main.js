43ab417c702d2c0d631bf6f7934a2b8ec2597148
gameztro100 <gameztro_100@hotmail.com>
2018-08-28 07:57:17 +0100

    create main.js
    created the main javascript file that handles the hamburger menu and side-menu

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function openSlideMenu() {
+  document.getElementById("side-menu").style.width = "250px";
+  document.getElementById("side-menu").style.display = "block";
+}
+
+function closeSlideMenu() {
+  document.getElementById("side-menu").style.width = "0";
+}
